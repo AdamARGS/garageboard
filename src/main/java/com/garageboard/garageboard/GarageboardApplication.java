df38d9e08c72e24197413af8d91e8fa0e0d4d18f
AdamARGS <aashafer@args.us>
2026-05-06 20:42:44 -0400

    Initial project setup

---
+++ src/main/java/com/garageboard/garageboard/GarageboardApplication.java
@@ -0,0 +1,13 @@
+package com.garageboard.garageboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garageboard/garageboard/GarageboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garageboard.garageboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
