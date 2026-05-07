e6c9f1555c91d94e7a65191473477a0fd55ff00b
AdamARGS <aashafer@args.us>
2026-05-06 22:30:53 -0400

    Added Security config + allow unauthenticated users to register

---
+++ src/main/java/com/garageboard/garageboard/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.garageboard.garageboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/users/register").permitAll()
+                .anyRequest().authenticated()
+            );
+        return http.build();
+    }
+    
+}
